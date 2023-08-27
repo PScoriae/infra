f2cd7cf1d11b4c7d92049636106071ecd25519ba
PScoriae <pierreccesario@gmail.com>
2023-08-27 14:55:37 +0800

    fix: remove gcp instances

--- gcp/main.tf
+++ gcp/main.tf
@@ -13,68 +13,69 @@ provider "google" {
   zone    = var.default_gcp_zone
 }
 
-resource "google_compute_address" "us_public_ip" {
-  name = "us-public-ip"
-}
+# resource "google_compute_address" "us_public_ip" {
+#   region = "us-west1"
+#   name   = "us-public-ip"
+# }
 
-resource "google_compute_address" "sg_public_ip" {
-  region = "asia-southeast1"
-  name   = "sg-public-ip"
-}
+# resource "google_compute_address" "sg_public_ip" {
+#   region = "asia-southeast1"
+#   name   = "sg-public-ip"
+# }
 
-resource "google_compute_instance" "k8s_control_plane1" {
-  name         = "k8s-control-plane1"
-  machine_type = var.k8s_machine_type
+# resource "google_compute_instance" "k8s_control_plane1" {
+#   name         = "k8s-control-plane1"
+#   machine_type = var.k8s_machine_type
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts-arm64"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-os-cloud/ubuntu-2204-lts-arm64"
+#     }
+#   }
 
-  network_interface {
-    network    = "default"
-    network_ip = "10.128.0.10"
-    access_config {
-      nat_ip = google_compute_address.us_public_ip.address
-    }
-  }
-}
+#   network_interface {
+#     network    = "default"
+#     network_ip = "10.128.0.10"
+#     access_config {
+#       nat_ip = google_compute_address.us_public_ip.address
+#     }
+#   }
+# }
 
-resource "google_compute_instance" "k8s_worker1" {
-  name         = "k8s-worker1"
-  machine_type = var.k8s_machine_type
+# resource "google_compute_instance" "k8s_worker1" {
+#   name         = "k8s-worker1"
+#   machine_type = var.k8s_machine_type
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts-arm64"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "ubuntu-os-cloud/ubuntu-2204-lts-arm64"
+#     }
+#   }
 
-  network_interface {
-    network    = "default"
-    network_ip = "10.128.0.20"
-    access_config {
-    }
-  }
-}
+#   network_interface {
+#     network    = "default"
+#     network_ip = "10.128.0.20"
+#     access_config {
+#     }
+#   }
+# }
 
-resource "google_compute_instance" "bastion_host" {
-  name         = "bastion-host"
-  machine_type = var.bastion_machine_type
-  zone         = "asia-southeast1-b"
+# resource "google_compute_instance" "bastion_host" {
+#   name         = "bastion-host"
+#   machine_type = var.bastion_machine_type
+#   zone         = "us-west1-b"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
 
-  network_interface {
-    network    = "default"
-    network_ip = "10.148.0.10"
-    access_config {
-      nat_ip = google_compute_address.sg_public_ip.address
-    }
-  }
-}
+#   network_interface {
+#     network    = "default"
+#     network_ip = "10.138.0.10"
+#     access_config {
+#       nat_ip = google_compute_address.us_public_ip.address
+#     }
+#   }
+# }
