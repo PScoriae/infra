7957babc07351cd998cce6251ce2804ec3c0cad5
PScoriae <pierreccesario@gmail.com>
2023-11-19 19:22:10 +0800

    feat: add more info gcp instance

--- gcp/main.tf
+++ gcp/main.tf
@@ -17,7 +17,7 @@ resource "google_compute_instance" "bastion_host" {
   name         = "bastion-host"
   machine_type = var.bastion_machine_type
   zone         = "us-central1-a"
-
+  tags         = ["http-server", "https-server"]
   boot_disk {
     initialize_params {
       type  = "pd-standard"
@@ -25,8 +25,16 @@ resource "google_compute_instance" "bastion_host" {
       size  = 10
     }
   }
-
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
   network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
     network    = "default"
     network_ip = "10.128.0.10"
   }
