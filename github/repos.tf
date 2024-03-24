58597ddaf000b87315c3855847105d8c7bfdce62
PScoriae <pierreccesario@gmail.com>
2024-03-24 21:35:18 +0800

    feat: import dotfiles and pcc repos

--- github/repos.tf
+++ github/repos.tf
@@ -8,3 +8,25 @@ resource "github_repository" "infra" {
   topics        = ["terraform", "aws", "gcp", "cloudflare", "github"]
   visibility    = "public"
 }
+
+resource "github_repository" "dotfiles" {
+  name          = "dotfiles"
+  description   = "Backup of all my dotfiles."
+  has_downloads = false
+  has_issues    = false
+  has_projects  = false
+  has_wiki      = false
+  topics        = ["dotfiles", "macos", "shell", "zsh"]
+  visibility    = "public"
+}
+
+resource "github_repository" "pierreccesario" {
+  name          = "pierreccesario"
+  description   = "My personal portfolio website built with Hugo, built with GitHub Actions and deployed on AWS."
+  has_downloads = false
+  has_issues    = false
+  has_projects  = false
+  has_wiki      = false
+  topics        = ["aws", "portfolio", "hugo", "cloudflare", "github", "github-actions"]
+  visibility    = "public"
+}
