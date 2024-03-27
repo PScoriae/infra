ca541ad197da9d2e93e6a85c5e4e171b7c2599df
PScoriae <pierreccesario@gmail.com>
2024-03-28 00:24:21 +0800

    fix: whitespace

--- aws/ap-southeast-1/s3/s3.tf
+++ aws/ap-southeast-1/s3/s3.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "tfstate" {
 resource "aws_s3_bucket" "shuttleday-payments" {
   bucket = "shuttleday-payments"
 }
+
 resource "aws_s3_bucket" "shuttleday-payments-staging" {
   bucket = "shuttleday-payments-staging"
 }
