cfa852b0520f0b9e2fac00ea3d6d7ca4887ff24b
PScoriae <contact@pierreccesario.com>
2024-03-30 10:59:09 +0800

    fix: decomm music site

--- aws/ap-southeast-1/s3-site/s3-site.tf
+++ aws/ap-southeast-1/s3-site/s3-site.tf
@@ -5,10 +5,3 @@ module "pcc" {
   cname_record       = "pierreccesario.com"
   site_redirect_from = "www.pierreccesario.com"
 }
-
-module "music_pcc" {
-  source = "../../../modules/s3_static_site"
-
-  cloudflare_zone_id = data.aws_ssm_parameter.cf_zone_id_pcc.value
-  cname_record       = "music.pierreccesario.com"
-}
