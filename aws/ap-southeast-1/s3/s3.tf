fff7c6855de7d4d0576f99051ff3e74bacd3f5ba
PScoriae <contact@pierreccesario.com>
2024-03-30 11:05:04 +0800

    fix: decomm shuttleday buckets

--- aws/ap-southeast-1/s3/s3.tf
+++ aws/ap-southeast-1/s3/s3.tf
@@ -1,11 +1,3 @@
 resource "aws_s3_bucket" "tfstate" {
   bucket = "pscoriae-tf-state-s3"
 }
-
-resource "aws_s3_bucket" "shuttleday-payments" {
-  bucket = "shuttleday-payments"
-}
-
-resource "aws_s3_bucket" "shuttleday-payments-staging" {
-  bucket = "shuttleday-payments-staging"
-}
