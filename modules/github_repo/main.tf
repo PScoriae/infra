1bb2300d4c537c24aa23979ab66d29220861c42b
PScoriae <contact@pierreccesario.com>
2024-03-31 12:44:56 +0800

    fix: specify github provider

--- modules/github_repo/main.tf
+++ modules/github_repo/main.tf
@@ -1,11 +1,8 @@
-resource "github_repository" "this" {
-  name          = var.name
-  description   = var.description
-  has_downloads = var.has_downloads
-  has_issues    = var.has_issues
-  has_projects  = var.has_projects
-  has_wiki      = var.has_wiki
-  topics        = var.topics
-  visibility    = var.visibility
-  archived      = var.archived
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
 }
