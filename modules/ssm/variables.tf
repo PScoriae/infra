ccee2a834e247fdfd086d268852c9ee1894a1cab
PScoriae <contact@pierreccesario.com>
2024-03-31 11:44:22 +0800

    feat: add description var to ssm module

--- modules/ssm/variables.tf
+++ modules/ssm/variables.tf
@@ -3,6 +3,12 @@ variable "name" {
   type        = string
 }
 
+variable "description" {
+  description = "The description of the parameter."
+  type        = string
+  default     = null
+}
+
 variable "type" {
   description = "The type of the parameter. Valid types are String, StringList, and SecureString."
   type        = string
