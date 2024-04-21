0892824d74097398e6ee9fee1f6027cd5cb5362e
PScoriae <contact@pierreccesario.com>
2024-04-21 21:03:25 +0800

    feat: add aws s3 bucket policy to module

---
+++ modules/s3/policy.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "public" {
+  statement {
+    sid = "PublicReadGetObject"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+  }
+}
--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -10,3 +10,9 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = var.public_access_block
   restrict_public_buckets = var.public_access_block
 }
+
+resource "aws_s3_bucket_policy" "public" {
+  count  = var.public_access_block ? 0 : 1
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.public.json
+}
