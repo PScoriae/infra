89e18f694cddc170be58f2313a8ec7b3dbefd0a0
PScoriae <contact@pierreccesario.com>
2024-05-01 15:48:17 +0800

    fix: update redirect

--- aws/ap-southeast-1/sso/permission-sets.tf
+++ aws/ap-southeast-1/sso/permission-sets.tf
@@ -2,8 +2,8 @@ resource "aws_ssoadmin_permission_set" "admin" {
   name             = "AdministratorAccess"
   description      = "POWAAAAAAH!"
   instance_arn     = tolist(data.aws_ssoadmin_instances.this.arns)[0]
-  relay_state      = "https://s3.console.aws.amazon.com/s3/home?region=ap-southeast-1#"
-  session_duration = "PT8H" # 8 hours
+  relay_state      = "https://ap-southeast-1.console.aws.amazon.com/console/home?region=ap-southeast-1#" # redirect to the console home
+  session_duration = "PT8H"                                                                              # 8 hours
 }
 
 resource "aws_ssoadmin_managed_policy_attachment" "admin" {
