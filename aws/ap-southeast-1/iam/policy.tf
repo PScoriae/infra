b684edabead3ea7df4ffa1ac7a19d3e28227df04
PScoriae <contact@pierreccesario.com>
2024-10-10 23:24:08 +0800

    feat: add gha role

--- aws/ap-southeast-1/iam/policy.tf
+++ aws/ap-southeast-1/iam/policy.tf
@@ -23,3 +23,27 @@ data "aws_iam_policy_document" "gha_assume_role_rw_s3_pcc" {
     }
   }
 }
+
+data "aws_iam_policy_document" "gha_assume_role_infra" {
+  statement {
+    sid     = "AllowGitHubActionsAssumeRole"
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:PScoriae/infra:ref:refs/heads/main"]
+    }
+  }
+}
--- aws/ap-southeast-1/iam/role.tf
+++ aws/ap-southeast-1/iam/role.tf
@@ -2,6 +2,10 @@ data "aws_iam_policy" "s3_pcc_rw" {
   name = "s3-pierreccesario.com-rw"
 }
 
+data "aws_iam_policy" "readonly" {
+  name = "ReadOnlyAccess"
+}
+
 resource "aws_iam_role_policy_attachment" "github_manage_s3_pcc" {
   role       = aws_iam_role.github_manage_s3_pcc.name
   policy_arn = data.aws_iam_policy.s3_pcc_rw.arn
@@ -11,4 +15,15 @@ resource "aws_iam_role" "github_manage_s3_pcc" {
   name               = "github-manage-s3-pierreccesario"
   assume_role_policy = data.aws_iam_policy_document.gha_assume_role_rw_s3_pcc.json
   description        = "Allow GitHub Actions to manage S3 bucket for pierreccesario.com"
+}
+
+resource "aws_iam_role_policy_attachment" "github_readonly_infra" {
+  role       = aws_iam_role.github_readonly_infra.name
+  policy_arn = data.aws_iam_policy.readonly.arn
+}
+
+resource "aws_iam_role" "github_readonly_infra" {
+  name               = "github-readonly-infra"
+  assume_role_policy = data.aws_iam_policy_document.gha_assume_role_infra.json
+  description        = "Allow GitHub Actions to read-only access to infra resources"
 }
