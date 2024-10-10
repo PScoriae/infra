04695a026bf8f3b670384d67397d852fe4d91cd6
PScoriae <contact@pierreccesario.com>
2024-10-10 23:24:15 +0800

    fix: decomm iam user

--- aws/ap-southeast-1/iam/user.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_iam_user" "terraform_admin" {
-  name = "terraform-admin"
-}
-
-resource "aws_iam_user_policy_attachment" "terraform_admin" {
-  user       = aws_iam_user.terraform_admin.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
