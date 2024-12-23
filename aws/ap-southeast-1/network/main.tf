ec1202e44e6a6a34c371d58edaf492a68fbe4fbe
PScoriae <contact@pierreccesario.com>
2024-12-23 22:15:49 +0800

    chore: move state

--- aws/ap-southeast-1/network/main.tf
+++ aws/ap-southeast-1/network/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     encrypt        = true
     bucket         = "pscoriae-tf-state-s3"
-    key            = "aws/ap-southeast-1/terraform.tfstate"
+    key            = "aws/ap-southeast-1/network/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "tf-state-lock"
   }
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region  = "ap-southeast-1"
-    default_tags {
+  default_tags {
     tags = {
       ManagedBy   = "Terraform"
       Environment = "Production"
