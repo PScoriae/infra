89ff5e2c2281c3473ff3ba911a17d7f8460110a3
PScoriae <contact@pierreccesario.com>
2025-02-18 23:15:54 +0800

    feat: import ecr scanning config

---
+++ aws/ap-southeast-1/ecr/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "pscoriae-tf-state-s3"
+    key            = "aws/ap-southeast-1/ecr/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "tf-state-lock"
+  }
+}
+
+provider "aws" {
+  region  = "ap-southeast-1"
+  default_tags {
+    tags = {
+      ManagedBy   = "Terraform"
+      Environment = "Production"
+    }
+  }
+}
