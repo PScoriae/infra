c060cecda565999f6604148e3b0b2eb8a8a746cf
PScoriae <pcesario@moneylion.com>
2026-07-25 01:27:23 +0800

    refactor(terraform): consolidate root config

--- aws/ap-southeast-1/dynamodb/terraform.tf
+++ aws/ap-southeast-1/dynamodb/terraform.tf
@@ -2,18 +2,25 @@ terraform {
   backend "s3" {
     encrypt        = true
     bucket         = "pscoriae-tf-state-s3"
-    key            = "aws/ap-southeast-1/budget/terraform.tfstate"
+    key            = "aws/ap-southeast-1/dynamodb/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "tf-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region = "ap-southeast-1"
   default_tags {
     tags = {
-      ManagedBy   = "Terraform"
       Environment = "Production"
+      ManagedBy   = "Terraform"
     }
   }
 }
--- aws/ap-southeast-1/s3/terraform.tf
+++ aws/ap-southeast-1/s3/terraform.tf
@@ -2,14 +2,21 @@ terraform {
   backend "s3" {
     encrypt        = true
     bucket         = "pscoriae-tf-state-s3"
-    key            = "aws/ap-southeast-1/ssm/terraform.tfstate"
+    key            = "aws/ap-southeast-1/s3/terraform.tfstate"
     region         = "ap-southeast-1"
     dynamodb_table = "tf-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
+  region = "ap-southeast-1"
   default_tags {
     tags = {
       ManagedBy   = "Terraform"
